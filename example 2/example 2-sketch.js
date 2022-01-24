e2825804d734e933a8360bbff7156050f6c97fb6
Catherine Men <catherinemen16@gmail.com>
2022-01-23 18:57:03 -0600

    example 2 done

---
+++ example 2/example 2-sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(255, 255, 255);
+
+  fill(255, 0, 0, 85);
+  noStroke();
+  circle(98, 70, 100);
+
+  fill(0, 255, 0, 85);
+  noStroke();
+  circle(135, 130, 100);
+
+  fill(0, 0, 255, 85);
+  noStroke();
+  circle(70, 130, 100);
+}
