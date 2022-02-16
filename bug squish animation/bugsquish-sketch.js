b0decfe19880005f0641b489af630625de4c58b7
Catherine Men <catherinemen16@gmail.com>
2022-02-15 22:07:25 -0600

    bg added

--- bug squish animation/bugsquish-sketch.js
+++ bug squish animation/bugsquish-sketch.js
@@ -1,7 +1,27 @@
+letbugImages = [];
+let imageURL = "https://catherine-men.github.io/pdmspring2022/bugsquishpics/";
+let testBug;
+
+function preload() {
+
+  for (let i = 0; i < 4; i++) {
+    bugImages[i] = loadImage(imageURL + "bugsprite_" + (i+1) + ".png");
+  }
+}
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+  testBug = createSprite(width / 2, height / 2, 50, 50);
+  testBug.oddAnimation("walk",
+                       bugImages[1],
+                       bugImages[2],
+                       bugImages[3],
+                       bugImages[4],
+  );
 }
 
 function draw() {
   background(220);
+  drawSprites();
+
 }
